70a82dd1f44ad9a53e1f9908f19510976ac7a8ba
Abhishek Choudhary <abhishekchoudhary@Abhisheks-MacBook-Pro.local>
2021-07-12 20:10:22 +0530

    basics of react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import "./style.css"
+import Button from "./Button"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Apple=()=>{
+    return(
+        <div>
+            <h1 style={{ textAlign : "center"}}> Hello world </h1>
+            <h2> Hello youtubers</h2> 
+            <Button buttonText="Apple"/>
+            <Button buttonText="Gauva"/>
+            <Button buttonText="Orange"/>
+       </div>
+       
+    )
 }
 
-export default App;
+export default Apple
